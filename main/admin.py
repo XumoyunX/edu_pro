110ce5fcf60e5336f5091eec88c258cc8993e9ed
unknown <XumoyunX>
2022-07-25 20:50:53 +0500

    asa

---
+++ main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from main.models import *
+
+
+admin.site.register(Team)
+admin.site.register(Coueres)
+admin.site.register(Coueres_price)
+admin.site.register(Team_bola)
---
+++ main/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from main.models import *
+
+def index(requesrt):
+    return render(requesrt, 'main/index.html')
+
+
+def course(requesrt):
+    return render(requesrt, 'main/kurslar.html')
+
+
+def course_lesson(request):
+    return render(request, 'main/kurs_dars.html')
+
+
+def rayteng(request):
+    student = Team_bola.objects.all()
+
+    ctx={
+        "student":student
+    }
+    return render(request, 'main/natija.html',ctx)
+
+
+def our_team(request):
+    return render(request, 'main/biz_jamo.html')
+
+
+def sign_up(request):
+    return render(request, 'main/sign_up.html')
+
+
+def sign_in(request):
+    return render(request, 'main/sign_in.html')
+
+
+def lesson(request):
+    return render(request, 'main/dars.html')
+
+
+
+def hello(request):
+    pass
